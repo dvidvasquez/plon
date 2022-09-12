0df24ee957de0844bfb64f863f0de3cc20cdfd1b
dvid <davidvasquez.s@hotmail.com>
2022-09-12 12:22:46 -0500

    cambio diseño y funcionalidad

--- index.js
+++ index.js
@@ -6,20 +6,48 @@ const clearValue = () => {
     num.innerHTML = 0;
 }
 
+const setBtn = (type) => {
+    const btn =  document.querySelector('button');
+    btn.className = ' ';
+    btn.setAttribute('disabled',false);
+    
+    if(type === 'rest'){
+        btn.classList.add('rest');
+        btn.setAttribute('disabled',true);
+        btn.innerHTML = 'Dejelo enfriar'
+    }
+
+    if(type === 'fire'){
+        btn.classList.add('fire');
+        btn.setAttribute('disabled',true);
+        btn.innerHTML = 'Calientelo'
+    }
+
+    if(type === 'default'){
+        btn.classList.add('default');
+        btn.innerHTML = 'Iniciar'
+    }
+  }
+
 const countDown = ()=>{
+    const btn =  document.querySelector('button');
     const num = document.querySelector('h1');
     val = num.innerHTML;
     num.innerHTML = parseInt(val) + 1;
-    if(val === "25"){
-        confirm("Alto al fuego");
+    if(val === "25" && btn.classList.contains('fire')){
+        setBtn('rest');
+        clearValue();
+        return false;
     }
-    if(val === "65"){
-        confirm("Fumele");
+    if(val === "45" && btn.classList.contains('rest')){
+        setBtn('default');
         clearInterval(interval);
         clearValue();
+        return false;
     }
   }
   
   const plon = () => {
-      interval = setInterval(countDown, 1000);
+    setBtn('fire');
+    interval = setInterval(countDown, 1000);
   }
